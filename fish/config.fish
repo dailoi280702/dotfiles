519784c6698d43e360d3fa5b5324d105860496f6
lloyd <dailoi280702@gmail.com>
2022-08-19 20:39:38 +0700

    hide vim cmd

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,7 @@ if status is-interactive
 	alias v="nvim"
 	alias vim="nvim"
 	alias vi="nvim"
-	alias vide="neovide"
+	# alias vide="neovide"
 	alias ll "exa -l -g --icons"
 	alias lla "ll -a"
 
@@ -34,3 +34,30 @@ export QT_IM_MODULE=ibus
 function nvm
     bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
 end
+
+set -U fish_color_autosuggestion      brblack
+set -U fish_color_cancel              -r
+set -U fish_color_command             brgreen
+set -U fish_color_comment             brmagenta
+set -U fish_color_cwd                 green
+set -U fish_color_cwd_root            red
+set -U fish_color_end                 brmagenta
+set -U fish_color_error               brred
+set -U fish_color_escape              brcyan
+set -U fish_color_history_current     --bold
+set -U fish_color_host                normal
+set -U fish_color_match               --background=brblue
+set -U fish_color_normal              normal
+set -U fish_color_operator            cyan
+set -U fish_color_param               brblue
+set -U fish_color_quote               yellow
+set -U fish_color_redirection         bryellow
+set -U fish_color_search_match        'bryellow' '--background=brblack'
+set -U fish_color_selection           'white' '--bold' '--background=brblack'
+set -U fish_color_status              red
+set -U fish_color_user                brgreen
+set -U fish_color_valid_path          --underline
+set -U fish_pager_color_completion    normal
+set -U fish_pager_color_description   yellow
+set -U fish_pager_color_prefix        'white' '--bold' '--underline'
+set -U fish_pager_color_progress      'brwhite' '--background=cyan'
