04b462a35b9ebe73d6b35316134dee46aabc00bf
Lloyd <dailoi280702@gmail.com>
2023-02-22 17:04:53 +0700

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -46,11 +46,11 @@ local wezterm = require("wezterm")
 -- 	return " " .. tab.tab_index + 1 .. " "
 -- end)
 
-local font_name = "victormono nerd font"
+local font_name = "liga sfmono nerd font"
 
 return {
 	default_cursor_style = "SteadyBar",
-	font = wezterm.font(font_name, { weight = "Medium" }),
+	font = wezterm.font(font_name, { weight = "Regular" }),
 	-- dpi = 74.0,
 	font_rules = {
 		{
@@ -69,22 +69,22 @@ return {
 		},
 	},
 	enable_tab_bar = false,
-	-- cell_width = 0.9,
-	-- line_height = 0.95,
-	font_size = 26,
+	cell_width = 0.95,
+	line_height = 0.95,
+	font_size = 25,
 	use_fancy_tab_bar = false,
-	window_background_opacity = 1,
+	window_background_opacity = 1.0,
 	window_close_confirmation = "NeverPrompt",
 	colors = require("colors.oxocarbon"),
 	window_decorations = "RESIZE",
 	window_padding = {
-		top = 10,
-		bottom = 10,
+		top = 20,
+		bottom = 0,
 		left = 10,
 		right = 10,
 	},
 	initial_cols = 100,
 	initial_rows = 30,
-	use_resize_increments = true,
+	-- use_resize_increments = true,
 	tab_max_width = 30,
 }
