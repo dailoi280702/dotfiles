f5c5fafc909fc46de79f021aef87fbf0dd0b88d3
Lloyd <dailoi280702@gmail.com>
2023-03-17 08:50:37 +0700

    wezterm: add split

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,14 +4,13 @@ local font_name = "liga sfmono nerd font"
 
 return {
 	default_cursor_style = "SteadyBar",
-	font = wezterm.font(font_name, { weight = "Regular" }),
-	-- dpi = 74.0,
+	font = wezterm.font(font_name, { weight = "Medium" }),
 	font_rules = {
 		{
 			intensity = "Bold",
 			font = wezterm.font({
 				family = font_name,
-				weight = "Bold",
+				weight = "Black",
 			}),
 		},
 		{
@@ -26,11 +25,10 @@ return {
 	hide_tab_bar_if_only_one_tab = true,
 	cell_width = 0.90,
 	line_height = 0.90,
-	font_size = 25,
+	font_size = 24,
 	use_fancy_tab_bar = false,
 	window_background_opacity = 1.0,
 	window_close_confirmation = "NeverPrompt",
-	-- colors = require("colors.oxocarbon"),
 	color_scheme = "Oxocarbon Dark",
 	window_decorations = "RESIZE",
 	window_padding = {
@@ -41,6 +39,41 @@ return {
 	},
 	initial_cols = 100,
 	initial_rows = 30,
-	-- use_resize_increments = true,
 	tab_max_width = 30,
+
+	inactive_pane_hsb = {
+		saturation = 1,
+		brightness = 1,
+	},
+
+	keys = {
+		{
+			key = "h",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.SplitPane({
+				direction = "Left",
+			}),
+		},
+		{
+			key = "l",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.SplitPane({
+				direction = "Right",
+			}),
+		},
+		{
+			key = "j",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.SplitPane({
+				direction = "Down",
+			}),
+		},
+		{
+			key = "k",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.SplitPane({
+				direction = "Up",
+			}),
+		},
+	},
 }
