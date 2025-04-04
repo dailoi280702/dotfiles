557287e04155c6306449820a58351a3a10b353d1
Lloyd <dailoi280702@gmail.com>
2025-04-04 14:08:37 +0700

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,9 @@
 set -gx PATH "$HOME/.cargo/bin" $PATH;
 set -gx PATH "$HOME/.local/bin" $PATH;
-set -gx GOBIN "$(go env GOPATH)/bin"
-set -gx PATH $GOBIN $PATH;
+set -gx GOROOT $(go env GOROOT)
+set -gx GOPATH $(go env GOPATH)
+set -gx GOBIN $(go env GOPATH)/bin
+set -gx PATH $PATH $GOBIN $GOPATH $GOROOT;
 
 # Only run this in interactive shells
 if status is-interactive
